9994ae231bcf629b5ca37a3e3e1a12782baaef0d
Bibek Khadka <bibekhadka@yahoo.com>
2018-09-14 21:30:22 +0545

    suggestions implemented

--- src/main/java/com/example/orchapi/common/config/ShiroConfig.java
+++ src/main/java/com/example/orchapi/common/config/ShiroConfig.java
@@ -4,17 +4,21 @@ import com.example.orchapi.filter.ApiGuardFilter;
 import com.example.orchapi.filter.JWTVerifyingFilter;
 import com.example.orchapi.filter.LimitedAccessJWTVerifyingFilter;
 import com.example.orchapi.logic.security.AdminRealm;
+import java.util.Map;
 import javax.servlet.Filter;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
 import org.apache.shiro.cache.CacheManager;
 import org.apache.shiro.cache.MemoryConstrainedCacheManager;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.realm.jdbc.JdbcRealm;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
 import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
 import org.apache.shiro.web.filter.authc.LogoutFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.apache.shiro.mgt.SecurityManager;
 
 @Configuration
 public class ShiroConfig {
@@ -78,6 +82,27 @@ public class ShiroConfig {
         chainDefinition.addPathDefinition("/api/**", "jwtv");
 
         return chainDefinition;
-    }   
+    }
+
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(@Value("#{ @environment['shiro.loginUrl'] ?: '/login.jsp' }") String loginUrl,
+            @Value("#{ @environment['shiro.successUrl'] ?: '/' }") String successUrl,
+            @Value("#{ @environment['shiro.unauthorizedUrl'] ?: null }") String unauthorizedUrl,
+            SecurityManager securityManager,
+            ShiroFilterChainDefinition shiroFilterChainDefinition,
+            Map<String, Filter> filterMap) {
+
+        ShiroFilterFactoryBean filterFactoryBean = new ShiroFilterFactoryBean();
+
+        filterFactoryBean.setLoginUrl(loginUrl);
+        filterFactoryBean.setSuccessUrl(successUrl);
+        filterFactoryBean.setUnauthorizedUrl(unauthorizedUrl);
+
+        filterFactoryBean.setSecurityManager(securityManager);
+        filterFactoryBean.setFilterChainDefinitionMap(shiroFilterChainDefinition.getFilterChainMap());
+        filterFactoryBean.setFilters(filterMap);
+
+        return filterFactoryBean;
+    }
 
 }
